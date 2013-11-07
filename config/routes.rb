0446ca967d1ac9b8b114b244230d41bcf1ee32ed
Ben Morales <b-morales@kellogg.northwestern.edu>
2013-11-06 23:24:19 -0600

    Multiple pictures - added 5 entry

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rxngif::Application.routes.draw do
 
-  get("/picture_details/:id", { :controller => "pictures", :action => "show" })
-  
+	get("/picture_details/:id", 		{ :controller => "pictures", :action => "show" })
+
+	get("/all_pictures", 				{ :controller => "pictures", :action => "index" })
+
 end
 
