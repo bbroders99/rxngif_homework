5f806eac960ea888573142dd098448f1997ece52
Ben Morales <b-morales@kellogg.northwestern.edu>
2013-11-06 23:41:27 -0600

    RCAV - 'create_picture'

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,18 +1,23 @@
 class PicturesController < ApplicationController
  
   def show
-
   	@id = params[:id]
   	@pic = Picture.find_by({ :id => @id })
   	@pic_source = @pic.source
   	@pic_caption = @pic.caption
-
   end
 
+
   def index
-
   	@pic_list = Picture.all
+  end
 
+
+  def new
+  end
+
+
+  def create
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rxngif::Application.routes.draw do
 
 	get("/all_pictures", 				{ :controller => "pictures", :action => "index" })
 
+	get("/new_picture_form", 			{ :controller => "pictures", :action => "new" })
+
+	get("/create_picture", 				{ :controller => "pictures", :action => "create" })
+
 end
 
