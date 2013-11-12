c0046a14f450f7a6889f4fab59a4d44e8a30f71e
Ben Morales <b-morales@kellogg.northwestern.edu>
2013-11-11 22:30:24 -0600

    Patch form

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rxngif::Application.routes.draw do
 
-	get("/picture_details/:id", 		{ :controller => "pictures", :action => "show" })
-
-	get("/all_pictures", 				{ :controller => "pictures", :action => "index" })
-
-	get("/new_picture_form", 			{ :controller => "pictures", :action => "new" })
-
+#CREATE
+	get("/new_picture_form", 			{ :controller => "pictures", :action => "new", :as => "new_picture" })
 	post("/create_picture", 			{ :controller => "pictures", :action => "create" })
 
-	get("/delete_picture/:id", 			{ :controller => "pictures", :action => "destroy" })
+# READ
+	get("/picture_details/:id", 		{ :controller => "pictures", :action => "show" })
+	get("/all_pictures", 				{ :controller => "pictures", :action => "index" })
 
+# UPDATE	
 	get("/edit_picture_form/:id", 		{ :controller => "pictures", :action => "edit" })
+	patch("/update_picture/:id", 		{ :controller => "pictures", :action => "update" })
 
-	get("/update_picture/:id", 			{ :controller => "pictures", :action => "update" })
+#DELETE
+	delete("/delete_picture/:id", 		{ :controller => "pictures", :action => "destroy" })
 
 
 end
