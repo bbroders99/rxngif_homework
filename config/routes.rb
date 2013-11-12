f805a1cafeb17409b0046aae79d32389bc4eb02e
Ben Morales <b-morales@kellogg.northwestern.edu>
2013-11-11 21:04:24 -0600

    Forms

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rxngif::Application.routes.draw do
 
 	get("/new_picture_form", 			{ :controller => "pictures", :action => "new" })
 
-	get("/create_picture", 				{ :controller => "pictures", :action => "create" })
+	post("/create_picture", 			{ :controller => "pictures", :action => "create" })
 
 	get("/delete_picture/:id", 			{ :controller => "pictures", :action => "destroy" })
 
